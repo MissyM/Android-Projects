6f9347bfa0de87b8b0aaf76e13886bce8b419b11
Milena Cabrera Patiño <mcabrerapatino@gmail.com>
2017-05-21 08:08:18 -0500

    Trabajando en algo grande

---
+++ ProyectoOlimpiadasMatematicas/app/src/main/java/com/milenacabrera/proyectoolimpiadasmatematicas/Splash_Activity.java
@@ -0,0 +1,13 @@
+package com.milenacabrera.proyectoolimpiadasmatematicas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Splash_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_);
+    }
+}
